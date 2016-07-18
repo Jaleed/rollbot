066998c6f5fc52f70c01ec4ce13a6cd9044a79df
mdszayy lmao <md.szyman@gmail.com>
2016-07-17 21:41:59 -0500

    remove active channels functionality, bot now works on all channels

--- rollbot.js
+++ rollbot.js
@@ -3,9 +3,6 @@ var fs = require('fs');
 
 var rollbot = new Discord.Client();
 
-// A list of channels that the bot will respond to commands in.
-var active_channels = ["dnd"];
-
 var roll = function(dice, sides) {
   var rolls = [];
   var min = 1;
@@ -25,37 +22,35 @@ var roll = function(dice, sides) {
 
 
 rollbot.on("message", function(message) {
-  if(active_channels.indexOf(message.channel.name) != -1) {
-    var channel_id = message.channel.id;
-    var match_data = message.content.match(/\/roll ([0-9]+)d([0-9]+)/);
-    if(match_data) {
-      n_dice = parseInt(match_data[1], 10);
-      n_sides = parseInt(match_data[2], 10);
-      var dice = roll(n_dice, n_sides);
-      if(!dice) {
-        rollbot.sendMessage(channel_id, "There's no reason you need those sorts of numbers.");
-        return;
-      }
-
-      var message_content = "";
-
-      if(n_dice > 1) {
-        var sum = dice.reduce(function(prev, curr) {
-          return prev + curr;
-        });
-
-        message_content = dice.join(", ") + " (" + sum + ")";
-      } else {
-        message_content = dice[0];
-      }
-
-
-      rollbot.sendMessage(channel_id, message_content);
+  var channel_id = message.channel.id;
+  var match_data = message.content.match(/\/roll ([0-9]+)d([0-9]+)/);
+  if(match_data) {
+    n_dice = parseInt(match_data[1], 10);
+    n_sides = parseInt(match_data[2], 10);
+    var dice = roll(n_dice, n_sides);
+    if(!dice) {
+      rollbot.sendMessage(channel_id, "There's no reason you need those sorts of numbers.");
+      return;
     }
+
+    var message_content = "";
+
+    if(n_dice > 1) {
+      var sum = dice.reduce(function(prev, curr) {
+        return prev + curr;
+      });
+
+      message_content = dice.join(", ") + " (" + sum + ")";
+    } else {
+      message_content = dice[0];
+    }
+
+
+    rollbot.sendMessage(channel_id, message_content);
   }
 });
 
-fs.readFile("auth.json", function(err, data) {
-  var auth_token =  JSON.parse(data).token;
+fs.readFile("config.json", function(err, data) {
+  var auth_token =  JSON.parse(data).discord_token;
   rollbot.loginWithToken(auth_token);
 });
